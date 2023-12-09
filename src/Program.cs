9236d9b245777cc7d1809d443eeec6fa43cec880
aaduser1 <aaduser1@contoso.com>
2023-12-09 05:37:15 +0000

    r

--- src/Program.cs
+++ src/Program.cs
@@ -1,25 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
-/*
-app.MapGet("/", () =>
-{
-    return File.ReadAllText("wwwroot/index.html"); 
-});
-
-app.MapFallback(context =>
-{
-    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
-    var content = File.ReadAllText(filePath);
-    return context.Response.WriteAsync(content);
-});
-*/
 
 app.Run();
