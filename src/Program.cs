e9778f55add6f847f8fe9655ba097a35eab28a4f
aaduser1 <128196860+aaduser1@users.noreply.github.com>
2023-12-09 04:39:58 +0000

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,33 @@
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services if needed
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync("Hello World!");
+});
 
 app.Run();
