187e85053635a3b13216268565856100a1126891
aaduser1 <128196860+aaduser1@users.noreply.github.com>
2023-12-09 05:00:11 +0000

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,11 @@ using System.IO;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseDefaultFiles();
+//app.UseDefaultFiles();
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    DefaultFileNames = new List<string> { "index.html" }
+});
 app.UseStaticFiles();
 
 app.MapGet("/", () =>
