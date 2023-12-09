d2637b7055ec666466ec42cb960281025f4fcd3d
aaduser1 <128196860+aaduser1@users.noreply.github.com>
2023-12-09 04:20:49 +0000

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 app.MapGet("/", () =>
 {
     return File.ReadAllText("wwwroot/index.html"); 
